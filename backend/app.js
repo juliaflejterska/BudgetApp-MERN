84fc04d639371a3f07ec1af918012526a62dac77
juliaflejterska <flejterskajulia@gmail.com>
2024-03-07 19:42:38 +0100

    backend structure added, server initialized

---
+++ backend/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+
+const server = () => {
+  app.listen(PORT, () => {
+    console.log("listening to port:", PORT);
+  });
+};
+
+server();
